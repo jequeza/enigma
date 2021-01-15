1d1eee353fe7ef9d93db37105f1021feb5285501
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-15 17:43:47 -0600

    Add test for attributes of Enigma class.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,7 +5,10 @@ class EnigmaTest < Minitest::Test
     @enigma = Enigma.new
   end
 
-  def test_it_exists
+  def test_it_exists_and_has_attributes
     assert_instance_of Enigma, @enigma
+    assert_equal ARGV[0], @enigma.file_in
+    assert_equal ARGV[1], @enigma.file_out
   end
 end
+
