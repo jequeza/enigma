3cec3b1cc8785b4d59cae25e2e58f1591e1da6f0
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-15 18:06:20 -0600

    Add #read to Enigma class,

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,6 +6,13 @@ class Enigma
     @file_in = ARGV[0]
     @file_out = ARGV[1]
   end
+
+  def read
+    handle = File.open(@file_in, "r")
+    text_in = handle.readlines
+    handle.close
+    text_in
+  end
 end
 
 
