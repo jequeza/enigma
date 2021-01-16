9c23dc76d034ce122db28db9692da93f2180d93b
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-15 18:05:41 -0600

    Add test for #read of Enigma class.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,8 +7,13 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     assert_instance_of Enigma, @enigma
-    assert_equal ARGV[0], @enigma.file_in
-    assert_equal ARGV[1], @enigma.file_out
+    assert_nil @enigma.file_in
+    assert_nil @enigma.file_out
+  end
+
+  def test_it_can_read_file
+    @enigma.stubs(:read).returns(["hello"])
+    assert_equal ["hello"], @enigma.read
   end
 end
 
