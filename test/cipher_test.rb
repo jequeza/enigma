f4e0410c2774f54b8b7a774e22d3dbb8e28b264b
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-15 21:16:14 -0600

    Add test for #five_digit_number of Cipher class.

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -10,4 +10,9 @@ class CipherTest < Minitest::Test
   def test_it_exists
     assert_instance_of Cipher, @cipher
   end
+
+  def test_it_can_generate_five_digit_number
+    @cipher.stubs(:five_digit_number).returns('23901')
+    assert_equal '23901', @cipher.five_digit_number
+  end
 end
