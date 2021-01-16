fd928db2930ca32a469264ed9dfd55d8512471e4
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-15 21:49:37 -0600

    Update Enigma class for attribute test, all tests pass.

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,11 +1,17 @@
 class Cipher
+  attr_reader :key,
+              :message
+
   def initialize(message)
     @message = message
+    @key = ""
   end
 
   def five_digit_number
     num = Array.new(5) { rand(0...9) }
-    num.join
+    num_joined = num.join
+    @key = num_joined
+    num_joined
   end
 
   def date
