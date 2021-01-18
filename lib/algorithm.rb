c4deee10029e1d8535e0206a71b7e0d80883926f
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-18 08:57:17 -0600

    Add #keys to Algorithm class, all tests pass.

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -2,4 +2,13 @@ class Algorithm
   def initialize(parent)
     @cipher = parent
   end
+
+  def keys
+    {
+      a_key: @cipher.key[0..1],
+      b_key: @cipher.key[1..2],
+      c_key: @cipher.key[2..3],
+      d_key: @cipher.key[3..4]
+    }
+  end
 end
