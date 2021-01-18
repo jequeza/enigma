b34f70cbf3fe55f22dc0ae40fec0b5292ac30fa2
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-18 08:56:47 -0600

    Add test for #keys of Algorithm class, changed to to store data as hash.

--- test/algorithm_test.rb
+++ test/algorithm_test.rb
@@ -1,9 +1,21 @@
 require "./test/test_helper"
 
 class AlgorithmTest < Minitest::Test
+  def setup
+    @parent = mock('cipher')
+    @algorithm = Algorithm.new(@parent)
+  end
+
   def test_it_exists_and_has_attributes
-    parent = mock('cipher')
-    algorithm = Algorithm.new(parent)
-    assert_instance_of Algorithm, algorithm
+    assert_instance_of Algorithm, @algorithm
+  end
+
+  def test_it_can_generate_keys
+    @parent.stubs(:key).returns('23901')
+    expected1 = {a_key: '23', b_key: '39', c_key: '90', d_key: '01'}
+    assert_equal expected1, @algorithm.keys
+    @parent.stubs(:key).returns('32871')
+    expected2 = {a_key: '32', b_key: '28', c_key: '87', d_key: '71'}
+    assert_equal expected2, @algorithm.keys
   end
 end
