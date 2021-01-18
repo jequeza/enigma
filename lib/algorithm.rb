e6dad14057759338cc8fabd6867f08e6fe62c04e
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-18 09:31:14 -0600

    Add #offsets to Algorithm class, all tests pass.

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -15,4 +15,13 @@ class Algorithm
   def square_date
     ((@cipher.date.to_i) ** 2).to_s
   end
+
+  def offsets
+    {
+      a_offset: square_date[-4].to_i,
+      b_offset: square_date[-3].to_i,
+      c_offset: square_date[-2].to_i,
+      d_offset: square_date[-1].to_i
+    }
+  end
 end
